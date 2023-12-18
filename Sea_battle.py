3153174b37ae57af6f328cf46013bd67d7a29591
kupruhin_av <kupruhin_av@KUPRUHINAV>
2023-12-18 19:45:05 +0300

    11

--- Sea_battle.py
+++ Sea_battle.py
@@ -2,11 +2,17 @@ import random
 import class_prog
 
 
-d = class_prog.Dot(1,2)
+# d = class_prog.Dot(1,2)
 
-print(d)
+# print(d)
 
-f = class_prog.Board(Bor=list(d))
+# f = class_prog.Board(Bor=list(d))
 
+if __name__ == "__main__":
 
+    dot = class_prog.Board().Bor3()
+
+    print(dot[4][2].__dir__())
+    print(dot[1][1].set_chip(True))
+    print(dot[1][1].chip)
 
--- class_prog.py
+++ class_prog.py
@@ -7,19 +7,49 @@ class Ship:
         self.orient = orient
         self.live = live
 
+    def get_live(self):
+        return self.live
     
-#класс доски
+    def get_orient(self):
+        return self.orient
+
+    
+
 class Dot:
-    def __init__(self, x,y):
+    def __init__(self,x,y,chip=False):
         self.x = x
         self.y = y
+        self.chip = chip
 
     def __eq__(self,other):
         return self.x==other.x and self.y==other.y
     
-class Board(Dot):
-    def __init__(self,Bor):
-        self.Bor = Bor
+    @property
+    def getX(self):
+        return self.x
+    
+    @property
+    def getY(self):
+        return self.y
+    
+    def set_chip(self,ship):
+        self.chip = ship
+    
+
+    
+#класс доски
+class Board:
+    def __init__(self):
+        self.Bor = [[]]
+
+    def Bor3(self):
+        self.Bor = [[]]
+        for a in range(7):
+            c = []
+            for b in range(7):
+                c.append(Dot(a,b))
+            self.Bor.append(c)
+        return self.Bor
 
     
 class Player:
